959d0636f9278bfe7a3e13dd67d4b43066a1a462
Duncan Marsh <dwolfm@gmail.com>
2016-09-01 15:06:05 -0700

    setup travis and heroku test script

--- webpack.config.js
+++ webpack.config.js
@@ -57,9 +57,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel',
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015'],
-        },
       },
       {
         test: /\.html$/,
